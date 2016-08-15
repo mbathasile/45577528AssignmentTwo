66a9a696b218238a9d2a4e7f6b01cefe62934038
Samkelo <55875564@mylife.unisa.ac.za>
2016-08-15 10:06:08 +0200

    Added GUI

--- src/RoadWarriors.UI/MainForm.vb
+++ src/RoadWarriors.UI/MainForm.vb
@@ -2,7 +2,7 @@
 Imports RoadWarriors.DataLayer
 
 Public Class MainForm
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         Dim f = New AthleteRepository()
 
         Dim athlete = New Athlete()
@@ -13,12 +13,50 @@ Public Class MainForm
 
         'MessageBox.Show("Returned value is : " & m.GetCheckDigit(athlete.BirthDate.ToString().Replace("/", "")))
 
-       ' MessageBox.Show("Returned value is : " & m.GetCheckDigit("19890105"))
+        MessageBox.Show("Returned value is : " & m.GetClubMembershipNumber("19890105"))
         Dim check = m.MembershipNumberValidator("15198901050010")
         If check Then
             MessageBox.Show("Yeppy !!! Found" & check)
         Else
             MessageBox.Show("Error this number dont exist" & check)
         End If
+
+
+    End Sub
+
+    Private Sub ManageAthletes_Btn_Click(sender As Object, e As EventArgs) Handles ManageAthletes_Btn.Click
+        Me.Hide()
+        Dim manageFrm = New ManageAthletes()
+        manageFrm.Show()
+    End Sub
+
+    Private Sub ManageEvents_Btn_Click(sender As Object, e As EventArgs) Handles ManageEvents_Btn.Click
+        Me.Hide()
+        Dim manageEvent = New ManageEvents()
+        manageEvent.Show()
+    End Sub
+
+    Private Sub AddAthleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddAthleteToolStripMenuItem.Click
+        Me.Hide()
+        Dim addAthlete = New AddAthleteFrm()
+        addAthlete.Show()
+    End Sub
+
+    Private Sub SearchEditToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SearchEditToolStripMenuItem.Click
+        Dim serchForm = New SearchForm()
+        serchForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub AddEventToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles AddEventToolStripMenuItem1.Click
+        Dim addEventForm = New AddEventForm()
+        Me.Hide()
+        addEventForm.Show()
+    End Sub
+
+    Private Sub EditEventToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditEventToolStripMenuItem.Click
+        Dim searchForm = New SearchEvent()
+        Me.Hide()
+        searchForm.Show()
     End Sub
 End Class
---
+++ src/RoadWarriors.UI/SearchForm.vb
@@ -0,0 +1,33 @@
+﻿Imports RoadWarriors.BusinessLogic
+Imports RoadWarriors.BusinessLogic.Logic
+Imports RoadWarriors.DataLayer
+Imports RoadWarriors.DataLayer.Repository
+
+Public Class SearchForm
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        Dim athleteRepo = New AthleteRepository()
+        Dim membershipValidator = New ClubMembershipNumberGenerator()
+
+        Dim isValidMember = False
+
+        If MembershipNumberTextBox.Text <> "" Then
+        isValidMember = membershipValidator.MembershipNumberValidator(MembershipNumberTextBox.Text)
+       End If
+
+        If isValidMember Then
+            Me.Hide()
+            Dim editAthleteFrm = New EditForm()
+            editAthleteFrm.GetValues(athleteRepo.GetAthleteBy(MembershipNumberTextBox.Text))
+            editAthleteFrm.Show()
+            Else
+            MsgBox("Member number is not valid please enter a valid number", MsgBoxStyle.Exclamation, "Error")
+        End If
+
+    End Sub
+
+    Private Sub BackButton_Click(sender As Object, e As EventArgs) Handles BackButton.Click
+        Me.Close()
+        Dim manageAthletesFrm = New ManageAthletes()
+        manageAthletesFrm.Show()
+    End Sub
+End Class
